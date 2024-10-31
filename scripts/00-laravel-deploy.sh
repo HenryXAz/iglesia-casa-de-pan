bbca41ea9ed7aa11ace4cbfc66af9fc8449a77cc
Henry Gustavo X Az <azhenrygus@outlook.es>
2024-10-30 20:11:21 -0600

    add allow composer env variable and optimization artisan commands to script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,15 @@
 echo "Running composer install..."
 composer install --no-dev -working-dir=/var/www/html
 
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
 echo "Running migrations..."
 php artisan migrate --force
 
 echo "Running seeders..."
 php artisan db:seed
+
